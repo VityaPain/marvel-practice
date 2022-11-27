1b34c1a5f673b948ab8078fd3068ebf5e0cdf7c8
VityaPain <vitayzablockyy58@gmail.com>
2022-11-28 00:18:52 +0300

    random character (+ transform data; - fixed)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -16,12 +16,25 @@ class MarvelService {
     }
 
     // запросы к API
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+        return res.data.results.map(this._transformCharacter) // каждый отдельный объект массива будет проходить через метод _transformCharacter
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`)
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    // возврат трансформированного объекта с нужными полями
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
